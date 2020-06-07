60bfec6650b86653e214c88c0e152664d06c3aa0
lucaspuglielli <lucaspuglielli@gmail.com>
2020-06-07 14:43:07 -0300

    display das informacoes no resumo e melhoria da logica de velidacao das marcacoes de horarios

--- controllers/pontoController.js
+++ controllers/pontoController.js
@@ -6,28 +6,28 @@ let pontoController = {
 		Ponto.salvarCheckin(matricula);
 		let teste = Ponto.listarPonto(matricula);
 		console.log(teste)
-		res.redirect("/resumo");
+		res.render("resumo", { title: "Resumo" , teste});
 	},
 	alterarLunchout: (req, res) => {
 		const { matricula } = req.body;
 		Ponto.salvarLunchout(matricula);
 		let teste = Ponto.listarPonto(matricula);
 		console.log(teste)
-		res.redirect("/resumo");
+		res.render("resumo", { title: "Resumo" , teste});
 	},
 	alterarLunchin: (req, res) => {
 		const { matricula } = req.body;
 		Ponto.salvarLunchin(matricula);
 		let teste = Ponto.listarPonto(matricula);
 		console.log(teste)
-		res.redirect("/resumo");
+		res.render("resumo", { title: "Resumo" , teste});
 	},
 	finalizarPonto: (req, res) => {
 		const { matricula } = req.body;
 		Ponto.salvarCheckout(matricula);
-		let teste = Ponto.listarPonto(matricula);
+		let {teste} = Ponto.listarPonto(matricula);
 		console.log(teste)
-		res.redirect("/resumo");
+		res.render("resumo", { title: "Resumo" , teste});
 	},
 };
 
